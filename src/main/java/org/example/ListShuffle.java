fb345b4f725f05a78dc634ebc2ecb72450cd44bb
amit maurya <amit@amit.maurya>
2023-08-25 12:13:59 +0530

    shuufle using random

--- src/main/java/org/example/ListShuffle.java
+++ src/main/java/org/example/ListShuffle.java
@@ -3,17 +3,34 @@ package org.example;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class ListShuffle {
 
     public static void main(String[] args) {
 
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        int[] arrayData = {1, 2, 3, 4, 5};
+
+        arrayData = listShuffle(arrayData);
 
         Collections.shuffle(data);
 
         System.out.println(data);
+        System.out.println(Arrays.toString(arrayData));
 
     }
+
+    private static int[] listShuffle(int[] arrayData) {
+        Random random = new Random();
+        for (int i = 0; i < arrayData.length ; i++) {
+            int randomIndex = random.nextInt(arrayData.length);
+            int temp = arrayData[randomIndex];
+            arrayData[randomIndex] = arrayData[i];
+            arrayData[i] = temp;
+        }
+        return arrayData;
+    }
+
 
 }
