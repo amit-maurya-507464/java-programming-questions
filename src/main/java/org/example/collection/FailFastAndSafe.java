18e4c837f72227f84363adcfe94311616549ec25
amit maurya <amit@amit.maurya>
2023-10-30 19:23:28 +0530

    fail fast and safe

---
+++ src/main/java/org/example/collection/FailFastAndSafe.java
@@ -0,0 +1,31 @@
+package org.example.collection;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class FailFastAndSafe {
+
+  public static void main(String[] args) {
+    failSafeIterator();
+    failFastIterator();
+  }
+	
+  private static void failSafeIterator() {
+    String[] data = {"Tomcat", "Undertow", "Kubernetes", "JBoss"};
+    List<String> servers = new CopyOnWriteArrayList<>(data);
+
+      for (String server : servers) {
+          System.out.println(server);
+          servers.add("Jetty");
+      }
+  }
+
+  private static void failFastIterator() {
+    List<String> servers = new ArrayList<>();
+    servers.add("Tomcat");
+      for (String server : servers) {
+          System.out.println(server);
+           servers.add("Jetty");
+      }
+  }
+}
